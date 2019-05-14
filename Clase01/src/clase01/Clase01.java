a8dd58158d210cc837b0c0c916386cf62b65de16
martinMamani <44142409+martinMamani@users.noreply.github.com>
2019-05-14 12:55:06 -0300

    Add files via upload

---
+++ Clase01/src/clase01/Clase01.java
@@ -0,0 +1,155 @@
+
+package clase01;
+/**
+ * esta clase es la principal del proyecto clase01
+ * @author martin
+ * 
+ * 
+ */
+
+public class Clase01 {
+
+    /**
+     * punto de entrada del proyecto
+     * 
+     * 
+     * @param args argumentos que ingresan de consola
+     */
+    
+    
+    
+    public static void main(String[] args) {
+    /*
+    //curso java standard programming 8.x 40hs
+    // dias: martes y jueves 10:00 a 13:00 hs.
+    // profe CarlosRios 
+    //mail: carlos.rios@educacionit.com
+    // repo: https://github.com/crios2020/javasemajuma
+    //materiales:alumni.educacionit.com
+    //user:mail pass:dni
+    }
+        software: jdk 8.x y netbeans:8.x
+        jdk: java development kit
+        lts: long term support 8 años.
+        
+        version         liberado           fin de soporte
+        
+        jdk 8 lts       marzo 2014         septiembre 2022
+        podemos descargarlo y usarlo y no se puede ver el codigo
+        jdk 9           septiembre 2017    marzo 2018
+        jdk 10          marzo 2018         septiembre 2018
+        jdk 11 lts      septiembre 2018    septiembre 2026
+        oracle deja descargar el producto para usar
+        cambio la forma de licenciamiento
+        nuestro cliente tiene que pagar el soporte 8 dolares por equipo
+        jdk 12          marzo 2019         septiembre 2019
+        jdk 13          septiembre 2019    marzo 2020
+        jdk 14 lts      marzo 2020         marzo 2028
+        
+        jdk alternativo: openjdk
+        
+        ide : integrated development enviroment
+        ide alternativos: eclipse jdeveloper intellij
+        
+        //comentario
+        // /* bloque */
+        // /** comentario java Doc
+        // estos deben ubicarse antes de definir un metodoo de clases.
+        
+    //Clase01.
+    
+    //tipo primitivos del lenguaje java
+    //tipos de datos boolean 1 byte
+    boolean bo = true;
+    System.out.println(bo);
+    bo= false;
+    System.out.println(bo);
+    
+    //tipos de datos bytes signed
+    
+    byte by=100;
+    System.out.println(by);
+    by= -100;
+    System.out.println(by);
+    
+    //tipos de datos short 2 bytes
+    
+    short sh= 2000;
+    System.out.println(sh);
+    sh=-2000;
+    System.out.println(sh);
+    
+    
+    //tipo de datos int 4 bytes
+    
+    int in= 2000000000;
+    System.out.println(in);
+    
+    //tipo de datos long 8 bytes
+    //ponemos la L para decir que la variable es de tipo long
+    long lo = 3000000000L;
+    System.out.println(lo);
+    
+    //tipo de datos char 2 bytes unsigned
+    
+    //devuelve un caracter
+    
+    char ch = 65;
+    System.out.println(ch);
+    ch+=32;
+    System.out.println(ch);
+    ch='c';
+    System.out.println(ch);
+    
+    
+    //tipo de datos float de 32 bits
+    
+    //ponemos la F para decir que la variable es de tipo float
+    float fl = 3.45F;
+    System.out.println(fl);
+    
+    //tipo de datos double 64 bits
+    
+    double dl = 3.46;
+    System.out.println(dl);
+    
+    fl=10;
+    dl=10;
+    
+    System.out.println(fl/3);
+    System.out.println(dl/3);
+    fl=100;
+    dl=100;
+    System.out.println(fl/3);
+    System.out.println(dl/3);
+    
+    //clase string 
+    
+    String texto = "hola";
+    System.out.println(texto);
+    
+    //en jdk8 la variable texto ,ocupa 8 bytes por que usa internamente un
+    // array char.
+    // en jdk 10 o superior la variable texto , ocupa 4 bytes por que internamente
+    // usa un array byte mas un archivo byte que indica cual es la tabla
+    // del lenguaje.
+    
+    //existe en java 9 
+    /*
+      tipo de datos var jdk 9 o superior
+      var var1=100;      //int
+      var1=200;          //ok
+      //var1=4.5;        //error
+    
+     var var2= 'a'     //char
+     var var3= true    //boolean
+     var var4= 4.65f;  //float
+     var var5 = 4.65;  //double
+     var var6 = "hola" //String
+    
+    
+    */
+    
+    } // fin de main
+}// fin de clase
+    
